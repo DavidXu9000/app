65cb1586887cb93aaeab9e762966486c87a6b009
David <david.duanmu.xu@utexas.edu>
2023-08-30 23:01:20 -0500

    upload audio

--- src/app/components/incident-detail/incident-detail.component.ts
+++ src/app/components/incident-detail/incident-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Cat } from 'src/app/common/cat';
@@ -12,29 +12,35 @@ import { IncidentService } from 'src/app/services/incident.service';
   styleUrls: ['./incident-detail.component.css']
 })
 export class IncidentDetailComponent implements OnInit {
+  selectedFile!:any
+
+  @ViewChild("fileinput", { static: false })
+  inputVar!: ElementRef;
+
   incident!: Incident;
   cat!: Cat;
-  createForm!: FormGroup;
+  createForm  : FormGroup = this.fb.nonNullable.group({
+      year: new FormControl(new Date().getFullYear(), [Validators.required, Validators.max(new Date().getFullYear())]),
+      day: new FormControl(new Date().getDay(), [Validators.required, Validators.min(1), Validators.max(31)]),
+      month: new FormControl(new Date().getMonth(), [Validators.required, Validators.min(1), Validators.max(12)])
+    })
 
   constructor(private catService: CatService, private incidentService: IncidentService, private route: ActivatedRoute, public fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    if (id === undefined || !Number.isInteger(id)) {
+    if (id === undefined || !Number.isInteger(parseInt(id!))) {
       this.router.navigateByUrl('/404');
       return;
     }
-    this.incidentService.getIncidentById(parseInt(id!)).subscribe((incident) => {
+     this.incidentService.getIncidentById(parseInt(id!)).subscribe((incident) => {
       this.incident = incident;
+      alert(incident.catId)
       this.catService.getCatById(this.incident.catId).subscribe((cat) => {
         this.cat = cat;
       })
     })
-    this.createForm = this.fb.nonNullable.group({
-      year: new FormControl(new Date().getFullYear(), [Validators.required, Validators.max(new Date().getFullYear())]),
-      day: new FormControl(new Date().getDay(), [Validators.required, Validators.min(1), Validators.max(31)]),
-      month: new FormControl(new Date().getMonth(), [Validators.required, Validators.min(1), Validators.max(12)])
-    })
+    
   }
 
   patchIncident() {
@@ -45,4 +51,12 @@ export class IncidentDetailComponent implements OnInit {
     this.incidentService.deleteIncident(this.incident);
   }
 
+  changeFile(event: any) {
+    this.selectedFile = event.target.files.item(0);
+  }
+
+  clearImage() {
+    this.selectedFile = '';
+    this.inputVar.nativeElement.value = "";
+  }
 }
